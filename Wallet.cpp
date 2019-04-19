6b7c3da5d06fb1eb86a4216b1c67714a0d24e5ef
Isam1234 <47877309+Isam1234@users.noreply.github.com>
2019-04-18 23:06:47 -0700

    Update Wallet.cpp

--- Wallet.cpp
+++ Wallet.cpp
@@ -6,19 +6,7 @@ using namespace std;
 
 Currency& Wallet::operator[] (const int i)
 {
-	return *(Curr[i]);
-}
-
-bool Wallet::isWalletEmpty() {
-	bool f = false;
-	for (int i = 0; i < ARRAY_SIZE; i++)
-	{
-		f = Curr[i]->is_Empty();
-		if (!f) {
-			return f;
-		}
-	}
-	return true;
+	return Curr[i];
 }
 
 int Wallet::moneyAmount()
@@ -27,7 +15,7 @@ int Wallet::moneyAmount()
 	int n = 0;
 	for (int i = 0; i < ARRAY_SIZE; i++)
 	{
-		f = Curr[i]->is_Empty();
+		f = Curr[i].is_Empty();
 		if (!f) {
 			n++;
 		}
@@ -36,26 +24,39 @@ int Wallet::moneyAmount()
 	return n;
 }
 
-bool Wallet::moneyAvailable(int cType) {
-	return !Curr[cType]->is_Empty();
-}
-
 void Wallet::gain(int curType, int w, int f)
 {
 	Currency tmp1("", w, f, "");
-	//curr[curType] = (curr[curType] + tmp1);
+	Curr[curType] = (Curr[curType] + tmp1);
 }
 
 void Wallet::spend(int curType, int w, int f)
 {
 	Currency tmp1("", w, f, "");
-	//curr[curType] = curr[curType] - tmp1;
-
-
+	Curr[curType] = (Curr[curType] - tmp1);
 }
+
+
+bool Wallet::moneyAvailable(int cType) {
+	return !Curr[cType].is_Empty();
+}
+
+
+bool Wallet::isWalletEmpty() {
+	bool f = false;
+	for (int i = 0; i < ARRAY_SIZE; i++)
+	{
+		f = Curr[i].is_Empty();
+		if (!f) {
+			return f;
+		}
+	}
+	return true;
+}
+
 void Wallet::empty(int curType) {
 	for (int i = 0; i < ARRAY_SIZE; i++)
 	{
-		(Curr[curType])->empty();
+		Curr[curType].empty();
 	}
 }
