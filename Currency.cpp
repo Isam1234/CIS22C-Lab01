75040c438f92856116cfab181b79611e8f21d7d2
Isam1234 <47877309+Isam1234@users.noreply.github.com>
2019-04-20 01:26:22 -0700

    Update Currency.cpp

--- Currency.cpp
+++ Currency.cpp
@@ -1,72 +1,66 @@
-#include <iostream>
-#include "Currency.h" 
 
-Currency::~Currency()
+#include "Currency.h"
+
+using namespace std;
+
+Currency::Currency(string wN, string fN, unsigned wV, unsigned fV) : wholeName(wN), fractName(fN), wholeVal(wV), fractVal(fV)
 {
+
 }
 
-void Currency::empty() {
-	Whole = 0;
-	Fract = 0;
-}
-
-bool Currency::is_Empty()
+Currency::Currency(string wN, string fN) : wholeName(wN), fractName(fN), wholeVal(0), fractVal(0)
 {
-	bool a = false;
-	if (Whole == 0 && Fract == 0)
-	{
-		a = true;
-	}
-	return a;
+
 }
 
-
-std::istream& operator >> (std::istream  &in, Currency &c1)
+// Mutators
+void Currency::setWholeName(std::string wN)
 {
-	in >> c1.Whole;
-	in >> c1.Fract;
-	return in;
+	wholeName = wN;
 }
 
-std::ostream& operator<<(std::ostream& out, const Currency& c2)
+void Currency::setFractName(std::string fN)
 {
-	out << c2.Cur << "," << c2.Whole << "," << c2.Fract << "," << c2.Coin;
-	return out;
+	fractName = fN;
 }
 
-Currency operator + (Currency const &c1, Currency const &c2) {
-	Currency cur;
-	cur.Whole = c1.Whole + c2.Whole;
-	cur.Fract = c1.Fract + c2.Fract;
-	if (cur.Fract >= 100)
-	{
-		cur.Whole = cur.Whole + (cur.Fract / 100);
-
-		cur.Fract = (cur.Fract % 100);
-	}
-	return cur;
+void Currency::setWholeVal(const unsigned wV)
+{
+	wholeVal = wV;
+}
+void Currency::setFractVal(const unsigned fV)
+{
+	fractVal = fV;
+}
+void Currency::setZero()
+{
+	wholeVal = 0;
+	fractVal = 0;
 }
 
-Currency operator - (Currency const &c1, Currency const &c2) {
-	Currency cur;
-	if (c1.Whole < c2.Whole) {
+// Accessors
+std::string Currency::getWholeName() const
+{
+	return wholeName;
+}
+std::string Currency::getFractName() const
+{
+	return fractName;
+}
+unsigned Currency::getWholeVal() const
+{
+	return wholeVal;
+}
+double Currency::getFractVal() const
+{
+	return fractVal;
+}
 
-		std::cout << "Error!!! There is not suficient money" << std::endl;
-	}
-	else if (c1.Whole == c2.Whole && c1.Fract < c2.Fract) {
-
-		std::cout << "Error!!! There is not suficient money" << std::endl;
-	}
-	else {
-		cur.Whole = c1.Whole - c2.Whole;
-		cur.Fract = c1.Fract - c2.Fract;
-
-		if (cur.Fract < 0)
-		{
-			cur.Whole = (cur.Whole - 1);
-
-			cur.Fract = ((c1.Fract + 100) - c2.Fract);
-		}
-	}
-	return cur;
+//function that returns a bool to check if currency is empty
+bool Currency::isZero() const
+{
+	if (wholeVal == 0 && fractVal == 0)
+		return true;
+	else
+		return false;
 }
