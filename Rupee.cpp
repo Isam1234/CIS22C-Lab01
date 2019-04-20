d8a1a0bcf5c5489a1431228e9377994f501a9c10
Isam1234 <47877309+Isam1234@users.noreply.github.com>
2019-04-20 01:32:14 -0700

    Create Rupee.cpp

---
+++ Rupee.cpp
@@ -0,0 +1,137 @@
+
+#include "Rupee.h"
+
+// Update whole and fractional parts
+void Rupee::updateCurrencyVal()
+{
+	if (fractVal > 100)
+	{
+		wholeVal += fractVal / 1000;
+
+		fractVal = fractVal % 1000;
+	}
+}
+
+//Operator overload implementation
+Rupee Rupee::operator + (const Rupee &x)
+{
+	Rupee newRupee;
+
+	newRupee.setWholeVal(this->wholeVal + x.getWholeVal());
+	newRupee.setFractVal(this->fractVal + x.getFractVal());
+	newRupee.updateCurrencyVal();
+
+	return newRupee;
+}
+Rupee Rupee::operator + (const double x)
+{
+	Rupee newRupee;
+
+	double fract, whole;
+
+	// Extract whole and fractional parts
+	fract = modf(x, &whole);
+	fract *= 100;
+	fract = round(fract);
+
+	// Whole part
+	newRupee.setWholeVal(this->wholeVal + static_cast<unsigned>(whole));
+
+	// Fractional part
+	newRupee.setFractVal(this->fractVal + static_cast<unsigned>(fract));
+
+	// Update currency values
+	newRupee.updateCurrencyVal();
+
+	return newRupee;
+}
+Rupee Rupee::operator - (const Rupee &x)
+{
+	Rupee newRupee;
+
+	newRupee.setWholeVal(this->wholeVal - x.getWholeVal());
+	newRupee.setFractVal(this->fractVal - x.getFractVal());
+	newRupee.updateCurrencyVal();
+
+	return newRupee;
+}
+Rupee Rupee::operator - (const double x)
+{
+	Rupee newRupee;
+
+	double fract, whole;
+
+	// Extract whole and fractional parts
+	fract = modf(x, &whole);
+	fract *= 100;
+	fract = round(fract);
+
+	// Whole part
+	newRupee.setWholeVal(this->wholeVal - static_cast<unsigned>(whole));
+
+	// Fractional part
+	newRupee.setFractVal(this->fractVal - static_cast<unsigned>(fract));
+
+	// Update currency values
+	newRupee.updateCurrencyVal();
+
+	return newRupee;
+}
+Rupee Rupee::operator = (const Rupee &x)
+{
+	Rupee newRupee;
+
+	newRupee.setWholeVal(x.getWholeVal());
+	newRupee.setFractVal(x.getFractVal());
+	newRupee.updateCurrencyVal();
+
+	return newRupee;
+}
+Rupee Rupee:: operator = (const double x)
+{
+	Rupee newRupee;
+	double fract, whole;
+
+	// Extract whole and fractional parts
+	fract = modf(x, &whole);
+	fract *= 100;
+	fract = round(fract);
+
+	// Whole part
+	newRupee.setWholeVal(static_cast<unsigned>(whole));
+
+	// Fractional part
+	newRupee.setFractVal(static_cast<unsigned>(fract));
+
+	// Update currency values
+	newRupee.updateCurrencyVal();
+
+	return newRupee;
+}
+
+std::ostream& operator << (std::ostream &out, const Rupee &x)
+{
+	double currencyValue = x.getWholeVal() + x.getFractVal();
+	out << to_string(currencyValue);
+	return out;
+}
+std::istream& operator >> (std::istream &in, Rupee &x)
+{
+	double fract,
+		whole,
+		val;
+
+	in >> val;
+
+	// Extract whole and fractional parts
+	fract = modf(val, &whole);
+	fract *= 100;
+	fract = round(fract);
+
+	Rupee newRupee;
+
+	x.setWholeVal(static_cast<unsigned>(whole) + x.getWholeVal());
+	x.setFractVal(fract + x.getFractVal());
+	x.updateCurrencyVal();
+	return in;
+}
