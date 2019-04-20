210beb8df922487693ff897d0ab31a9f2cb94a0c
Isam1234 <47877309+Isam1234@users.noreply.github.com>
2019-04-20 01:31:46 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,252 +1,331 @@
-//************************************************************************************
-//  Lab 1 - Wallet Program
-//  Isa Muran (+1) & Laurie Hupman
-//
-//  
-//  Program to simulate a wallet while on a foreign trip.  The wallet holds five
-//    types of foreign currency:  dollars, euros, yen, rupees and yuan.  It will
-//    display how much of each type of currency is currently available, add, subtract,
-//    and empty the contents.
-//
-//*************************************************************************************
 
-#include "Wallet.h"
-#include "Currency.h"
 #include <iostream>
-#include <string>
+#include <iomanip>
+#include "Wallet.h"
+
 using namespace std;
 
-int main() 
+void addMoney(Wallet&);
+void subtractMoney(Wallet&);
+void viewWallet(Wallet&);
+void emptyWallet(Wallet&);
 
+/*
+******************************************
+*           Psuedocode Main              *
+******************************************
+Declare varibles to input choice from user
+Diplay menu and take user input 
+Switch to perform user request
+if choice is 1
+call add money function
+if choice is 2
+call subtract money function
+if choice is 3
+call function that display money
+if choice is 4
+call function which removes all money 
+if choice is 5
+close program
+else
+display error
+*/
+
+int main()
 {
-	// Define the currency class
-	Currency *currency[ARRAY_SIZE];
-	currency[DOLLAR] = new Dollar();
-	currency[EURO] = new Euro();
-	currency[YUAN] = new Yuan();
-	currency[RUPEE] = new Rupee();
-	currency[YEN] = new Yen();
+	Wallet myWallet;
 
-	// Define the wallet class
-	Wallet w;
-	w[DOLLAR] = *(currency[DOLLAR]);
-	w[EURO] = *(currency[EURO]);
-	w[YUAN] = *(currency[YUAN]);
-	w[RUPEE] = *(currency[RUPEE]);
-	w[YEN] = *(currency[YEN]);
+	bool loop = false;
 
-	// The main menu
-	string in;
-	while (true) {
-		cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
-		cout << "\t\t\tWallet" << endl;
-		cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
-		char choice;
-		cout << "---------------------------" << endl;
-		cout << "(1)View Amounts of Currency" << endl;
-		cout << "(2)Add Money               " << endl;
-		cout << "(3)Remove Money            " << endl;
-		cout << "(4)Zero out Money          " << endl;
-		cout << "(5)Report                  " << endl;
-		cout << "(6)Exit                    " << endl;
-		cout << "---------------------------" << endl;
-		cout << "Enter Choice(1,2,3,4,5,6):";
+	while (loop == false)
+	{
+		int choice = 0;
+
+		cout << endl << "	***YOUR WALLET***" << endl << endl;
+		cout << "	 1: Add Money" << endl;
+		cout << "	 2: Subtract Money" << endl;
+		cout << "	 3: View Wallet" << endl;
+		cout << "	 4: Empty Wallet" << endl;
+		cout << "	 5: EXIT" << endl << endl;
+		cout << "	 Choice: ";
+
 		cin >> choice;
 		
-		// Display the contents of the wallet
-		if (choice == '1') {
-			cout << "---------------------------" << endl;
-			cout << "Currency Held: " << endl;
-			cout << "Dollars: " << w[DOLLAR] << endl;
-			cout << "Euros:   " << w[EURO] << endl;
-			cout << "Yen :    " << w[YEN] << endl;
-			cout << "Rupee:   " << w[RUPEE] << endl;
-			cout << "Yuan:    " << w[YUAN] << endl;
 
-		}
-		
-		// Add money to the wallet
-		if (choice == '2') {
-			char choice2 = 'y';
-			do {
-				cout << "---------------------------" << endl;
-				cout << "Currency Held: " << endl;
-				cout << "Dollars: " << w[DOLLAR] << endl;
-				cout << "Euros:   " << w[EURO] << endl;
-				cout << "Yen:     " << w[YEN] << endl;
-				cout << "Rupee:   " << w[RUPEE] << endl;
-				cout << "Yuan:    " << w[YUAN] << endl;
+		switch (choice)
+		{
+		case 1:
+			system("cls");
+			addMoney(myWallet);
+			system("cls");
+			break;
 
-				string type;
-				int amnt;
-				int decAmnt;
-				cout << "Type of Currency(USD,YEN,EUR,RUP,YUN)?:";
-				cin >> type;
-				if (type == "USD") {
-					cout << "enter Dollar amount: ";
-					cin >> amnt;
-					cout << "enter Cents amount: ";
-					cin >> decAmnt;
-					w.gain(DOLLAR, amnt, decAmnt);
-				}
-				else if (type == "EUR") {
-					cout << "enter Euro amount: ";
-					cin >> amnt;
-					cout << "enter Cents amount: ";
-					cin >> decAmnt;
-					w.gain(EURO, amnt, decAmnt);
-				}
-				else if (type == "YUAN") {
-					cout << "enter Yuan amount: ";
-					cin >> amnt;
-					cout << "enter fen amount: ";
-					cin >> decAmnt;
-					w.gain(YUAN, amnt, decAmnt);
-				}
-				else if (type == "RUP") {
-					cout << "enter rupee amount: ";
-					cin >> amnt;
-					cout << "enter paise amount: ";
-					cin >> decAmnt;
-					w.gain(RUPEE, amnt, decAmnt);
-				}
-				else if (type == "YEN") {
-					cout << "enter yen amount: ";
-					cin >> amnt;
-					cout << "enter sen amount: ";
-					cin >> decAmnt;
-					w.gain(YEN, amnt, decAmnt);
-				}
-				else {
-					cout << "Choice not recognized" << endl;
-				}
-				cout << "add more money?(y/n):";
-				cin >> choice2;
+		case 2:
+			system("cls");
+			subtractMoney(myWallet);
+			system("cls");
+			break;
 
-			} while (choice2 == 'y' || choice2 == 'Y');
-			choice2 = 'y';
-		}
+		case 3:
+			system("cls");
+			viewWallet(myWallet);
+			system("cls");
+			break;
 
-		// Subtract money from the wallet
-		else if (choice == '3') {
-			cout << "---------------------------" << endl;
-			cout << "Currency Held: " << endl;
-			cout << "Dollars: " << w[DOLLAR] << endl;
-			cout << "Euros:   " << w[EURO] << endl;
-			cout << "Yen:     " << w[YEN] << endl;
-			cout << "Rupee:   " << w[RUPEE] << endl;
-			cout << "Yuan:    " << w[YUAN] << endl;
+		case 4:
+			system("cls");
+			emptyWallet(myWallet);
+			system("cls");
+			break;
 
-			string type;
-			int amnt;
-			int decAmnt;
+		case 5:
+			loop = true;
+			break;
 
-			cout << "Type of Currency(USD,YEN,EUR,RUP,YUN)?:";
-			cin >> type;
-			if (type == "USD") {
-				cout << "enter Dollar amount: ";
-				cin >> amnt;
-				cout << "enter Cents amount: ";
-				cin >> decAmnt;
-				w.spend(DOLLAR, amnt, decAmnt);
-			}
-			else if (type == "EUR") {
-				cout << "enter Euro amount: ";
-				cin >> amnt;
-				cout << "enter Cents amount: ";
-				cin >> decAmnt;
-				w.spend(EURO, amnt, decAmnt);
-			}
-			else if (type == "YUN") {
-				cout << "enter Yuan amount: ";
-				cin >> amnt;
-				cout << "enter fen amount: ";
-				cin >> decAmnt;
-				w.spend(YUAN, amnt, decAmnt);
-			}
-			else if (type == "RUP") {
-				cout << "enter rupee amount: ";
-				cin >> amnt;
-				cout << "enter paise amount: ";
-				cin >> decAmnt;
-				w.spend(RUPEE, amnt, decAmnt);
-			}
-			else if (type == "YEN") {
-				cout << "enter yen amount: ";
-				cin >> amnt;
-				cout << "enter sen amount: ";
-				cin >> decAmnt;
-				w.spend(YEN, amnt, decAmnt);
-			}
-			else {
-				cout << "Choice not recognized" << endl;
-			}
-		}
-
-		// Empty part or all of the wallet
-		else if (choice == '4') {
-			char choice3;
-			cout << "(1)Dollars" << endl;
-			cout << "(2)Euro   " << endl;
-			cout << "(3)Yen    " << endl;
-			cout << "(4)Rupee  " << endl;
-			cout << "(5)Yuan   " << endl;
-			cout << "(6)All    " << endl;
-			cout << "Enter which Currency type you wish to zero out: ";
-			cin >> choice3;
-			switch (choice3) {
-			case '1':
-				w.empty(DOLLAR);
-				break;
-			case '2':
-				w.empty(EURO);
-				break;
-			case '3':
-				w.empty(YEN);
-				break;
-			case '4':
-				w.empty(RUPEE);
-				break;
-			case '5':
-				w.empty(YUAN);
-				break;
-			case '6':
-				w.empty(DOLLAR);
-				w.empty(EURO);
-				w.empty(YEN);
-				w.empty(RUPEE);
-				w.empty(YUAN);
-				break;
-			}
-			cout << "---------------------------" << endl;
-			cout << "Currency Held: " << endl;
-			cout << "Dollars: " << w[DOLLAR] << endl;
-			cout << "Euro:    " << w[EURO] << endl;
-			cout << "Yen:     " << w[YEN] << endl;
-			cout << "Rupee:   " << w[RUPEE] << endl;
-			cout << "Yuan:    " << w[YUAN] << endl;
-
-		}
-		else if (choice == '5') {
-			cout << "---------------------------" << endl;
-			cout << "Currency Held: " << endl;
-			cout << "Dollars: " << w[DOLLAR] << endl;
-			cout << "Euros:   " << w[EURO] << endl;
-			cout << "Yen:     " << w[YEN] << endl;
-			cout << "Rupee:   " << w[RUPEE] << endl;
-			cout << "Yuan:    " << w[YUAN] << endl;
+		default:
+			cout << "\t\t**Please enter a valid choice**\n" << endl; //error message when choice is not from the menu
+			break;
 
 		}
 
-		// Exit the program
-		else if (choice == '6') {
-		break;
-		}
+		system("cls"); //clear screen 
 
-		// Error for invalid choice
-		else {
-			cout << "choice not recognized :))" << endl;
-		}
 	}
+	
 	return 0;
+}
+
+/*
+******************************************
+*           Add Money Function           *
+******************************************
+Declare varibles to input choice from user
+Diplay menu and take user input
+Switch to perform user request
+if choice is 1
+take input value
+add input to currency 1
+if choice is 2
+take input value
+add input to currency 2
+if choice is 3
+take input value
+add input to currency 3
+if choice is 4
+take input value
+add input to currency 4
+if choice is 5
+take input value
+add input to currency 5
+if choice is 6
+return to previous menu
+else
+display error
+*/
+
+void addMoney(Wallet &myWallet)
+{
+
+	bool loop = false;
+
+	while (loop == false)
+	{
+		int choice = 0;
+
+		double currency = 0;
+
+		cout << endl << "	***Add Money***" << endl << endl
+			<< "	 1: Dollar" << endl
+			<< "	 2: Rupee" << endl
+			<< "	 3: Euro" << endl
+			<< "	 4: Yen" << endl
+			<< "	 5: Yuan" << endl
+			<< "	 6: Exit" << endl << endl
+			<< "	 Choice: ";
+		cin >> choice;
+
+
+		switch (choice)
+		{
+		case 1:
+			cout << "How much would you like to add? (Format: n.nn)\n";
+			cin >> currency;
+			myWallet.addMoney(Wallet::DOLLAR, currency);
+
+			cout << "Added " << fixed << setprecision(2) << currency << " in dollars to wallet\n" << endl;
+			break;
+
+		case 2:
+			cout << "How much would you like to add? (Format: n.nn)\n";
+			cin >> currency;
+			myWallet.addMoney(Wallet::RUPEE, currency);
+
+			cout << "Added " << fixed << setprecision(2) << currency << " in Rupee to wallet\n" << endl;
+			break;
+
+		case 3:
+			cout << "How much would you like to add? (Format: n.nn)\n";
+			cin >> currency;
+			myWallet.addMoney(Wallet::EURO, currency);
+
+			cout << "Added " << fixed << setprecision(2) << currency << " in euros to wallet\n" << endl;
+			break;
+
+		case 4:
+			cout << "How much would you like to add? (Format: n.nn)\n";
+			cin >> currency;
+			myWallet.addMoney(Wallet::YEN, currency);
+
+			cout << "Added " << fixed << setprecision(2) << currency << " in Yen to wallet\n" << endl;
+			break;
+
+		case 5:
+			cout << "How much would you like to add? (Format: n.nn)\n";
+			cin >> currency;
+			myWallet.addMoney(Wallet::YUAN, currency);
+
+			cout << "Added " << fixed << setprecision(2) << currency << " in Yuan to wallet\n" << endl;
+			break;
+
+		case 6:
+			loop = true;
+			break;
+
+		default:
+			cout << "\t\t**Please enter a valid choice**\n" << endl;
+			break;
+
+		}
+
+	}
+}
+
+/*
+******************************************
+*      Subtract Money Function           *
+******************************************
+Declare varibles to input choice from user
+Diplay menu and take user input
+Switch to perform user request
+if choice is 1
+take input value
+Subtract input from currency 1
+if choice is 2
+take input value
+Subtract input from currency 2
+if choice is 3
+take input value
+Subtract input from currency 3
+if choice is 4
+take input value
+Subtract input from currency 4
+if choice is 5
+take input value
+Subtract input from currency 5
+if choice is 6
+return to previous menu
+else
+display error
+*/
+
+void subtractMoney(Wallet &myWallet)
+{
+	bool loop = false;
+
+	while (loop == false)
+	{
+		int choice = 0;
+
+		double currency = 0;
+
+		cout << endl << "	***Subtract Money***" << endl << endl
+			<< "	 1: Dollar" << endl
+			<< "	 2: Rupee" << endl
+			<< "	 3: Euro" << endl
+			<< "	 4: Yen" << endl
+			<< "	 5: Yuan" << endl
+			<< "	 6: Exit" << endl << endl
+			<< "	 Choice: ";
+		cin >> choice;
+
+
+		switch (choice)
+		{
+		case 1:
+			cout << "How much would you like to subtract? (Format: n.nn)\n";
+			cin >> currency;
+			myWallet.subtractMoney(Wallet::DOLLAR, currency);
+
+			cout << "Subtracted " << fixed << setprecision(2) << currency << " in dollars to wallet\n" << endl;
+			break;
+
+		case 2:
+			cout << "How much would you like to subtract? (Format: n.nn)\n";
+			cin >> currency;
+			myWallet.subtractMoney(Wallet::RUPEE, currency);
+
+			cout << "Subtracted " << fixed << setprecision(2) << currency << " in Rupee to wallet\n" << endl;
+			break;
+
+		case 3:
+			cout << "How much would you like to subtract? (Format: n.nn)\n";
+			cin >> currency;
+			myWallet.subtractMoney(Wallet::EURO, currency);
+
+			cout << "Subtracted " << fixed << setprecision(2) << currency << " in euros to wallet\n" << endl;
+			break;
+
+		case 4:
+			cout << "How much would you like to subtract? (Format: n.nn)\n";
+			cin >> currency;
+			myWallet.subtractMoney(Wallet::YEN, currency);
+
+			cout << "Subtracted " << fixed << setprecision(2) << currency << " in Yen to wallet\n" << endl;
+			break;
+
+		case 5:
+			cout << "How much would you like to subtract? (Format: n.nn)\n";
+			cin >> currency;
+			myWallet.subtractMoney(Wallet::YUAN, currency);
+
+			cout << "Subtracted " << fixed << setprecision(2) << currency << " in Yuan to wallet\n" << endl;
+			break;
+
+		case 6:
+			loop = true;
+			break;
+
+		default:
+			cout << "\t\t**Please enter a valid choice**\n" << endl;
+			break;
+
+		}
+
+	}
+}
+
+//Get values from wallet class and display them on screen
+void viewWallet(Wallet &myWallet)
+{
+	cout << endl << "	***Viewing Wallet***" << endl << endl
+		<< "	 1: Dollars: " << fixed << setprecision(2) << myWallet.getMoney(Wallet::DOLLAR) << "\n"
+		<< "	 2: Rupee: " << fixed << setprecision(2) << myWallet.getMoney(Wallet::RUPEE) << "\n"
+		<< "	 3: Euro: " << fixed << setprecision(2) << myWallet.getMoney(Wallet::EURO) << "\n"
+		<< "	 4: Yen: " << fixed << setprecision(2) << myWallet.getMoney(Wallet::YEN) << "\n"
+		<< "	 5: Yuan: " << fixed << setprecision(2) << myWallet.getMoney(Wallet::YUAN) << "\n"
+		<< endl << endl;
+
+	system("pause");
+}
+
+//Calls function from wallet class to set all values to zero
+void emptyWallet(Wallet &myWallet)
+{
+	cout << endl << "	***Emptying Wallet...***" << endl;
+	
+	myWallet.emptyWallet();
+	
+	cout << endl << "	***Wallet Emptied***" << endl;
+
+	system("pause");
 }
