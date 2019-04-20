48dc680b4bedadca389f5296040baf027e99c807
Isam1234 <47877309+Isam1234@users.noreply.github.com>
2019-04-20 01:27:19 -0700

    Create Dollar.h

---
+++ Dollar.h
@@ -0,0 +1,44 @@
+
+#ifndef DOLLAR_H
+#define DOLLAR_H
+
+#include "Currency.h"
+#include <string>
+
+using namespace std;
+
+//Dollar class inherited form currency class
+class Dollar : public Currency
+{
+private:
+
+
+public:
+
+	//Constructors
+	Dollar(unsigned wV, unsigned fV) : Currency("Dollar", "Cent", wV, fV)
+	{
+
+	}
+
+	Dollar() : Currency("Dollar", "Cent")
+	{
+
+	}
+
+	void updateCurrencyVal();
+
+	// Overloaded Operators
+	Dollar operator + (const Dollar &);
+	Dollar operator + (const double);
+	Dollar operator - (const Dollar &);
+	Dollar operator - (const double);
+	Dollar operator = (const Dollar &);
+	Dollar operator = (const double);
+
+	friend std::ostream& operator << (std::ostream &, const Dollar &);
+	friend std::istream& operator >> (std::istream &, Dollar &);
+
+};
+
+#endif
