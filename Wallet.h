1d19c9cb52d81c7ff0a9c1461d85f908ab79b97c
Isam1234 <47877309+Isam1234@users.noreply.github.com>
2019-04-20 01:28:35 -0700

    Update Wallet.h

--- Wallet.h
+++ Wallet.h
@@ -1,14 +1,49 @@
-#pragma once
+
+#ifndef WALLET_H
+#define WALLET_H
+
+#include <string>
+#include <iostream>
+
 #include "Currency.h"
+#include "Dollar.h"
+#include "Rupee.h"
+#include "Euro.h"
+#include "Yen.h"
+#include "Yuan.h"
+
 class Wallet
 {
-	Currency Curr[ARRAY_SIZE];
+private:
+	// Currency Pointer for each currency type
+	Currency *cPtr[5];
+
 public:
-	Currency & operator[] (const int index);
-	int moneyAmount();
-	void gain(int, int, int);
-	void spend(int, int, int);
-	bool moneyAvailable(int);
-	bool isWalletEmpty();
-	void empty(int);
+
+	//enumertaors to hold currency types
+	static enum currencyFlag
+	{
+		DOLLAR,
+		RUPEE,
+		EURO,
+		YEN,
+		YUAN
+	};
+
+	//defaullt constructor
+	Wallet();
+
+	//default destructor
+	~Wallet();
+
+	int numOfCurrencies() const;
+	bool checkCurrencyExist(currencyFlag);
+	void addMoney(currencyFlag, const double);
+	void subtractMoney(currencyFlag, const double);
+	double getMoney(currencyFlag);
+	void emptyWallet();
+	bool checkIfEmpty();
+
 };
+
+#endif
