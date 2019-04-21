d79e0d3accac9f774b38c3d735469b7c5cb0d69c
Isam1234 <47877309+Isam1234@users.noreply.github.com>
2019-04-20 22:33:25 -0700

    Update Currency.cpp

--- Currency.cpp
+++ Currency.cpp
@@ -9,7 +9,15 @@ Mutators and accessors
 #include "Currency.h"
 
 using namespace std;
-
+/*
+Psuedocode for currency definitions:
+Create a constructor that will take two strings for names and two numbers for currency
+Have a default constructor in case the numbers were omitted
+Setters will take arguments and set the value in the argument to the value in the class
+Set zero will zero out all the values in the currency class.
+The getters will return the values stored in the currency class.
+the function that will check if all values in the wallet class are zero will return a true or false statement.
+*/
 Currency::Currency(string wN, string fN, unsigned wV, unsigned fV) : wholeName(wN), fractName(fN), wholeVal(wV), fractVal(fV)
 {
 
