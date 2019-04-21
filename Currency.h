8dbbf5ddb28fd4626437a0ccf48a7a6d05e188d2
Isam1234 <47877309+Isam1234@users.noreply.github.com>
2019-04-20 22:39:09 -0700

    Update Currency.h

--- Currency.h
+++ Currency.h
@@ -10,7 +10,16 @@ Definitions for the Currency class
 
 #include <string>
 #include <iostream>
-
+/*
+Psuedocode for currency class:
+The currency class will have values that can only be accessed by methods 
+these attributes will be name, amount in whole, amount in fraction, and fraction name
+The class needs constructors, one default, one with parameters.
+The setters will take an argument that will be used for setting values in the class.
+There will be an empty method that will set all the values to zero
+there will be getters that will return all the values in the class.
+The function that will tell the program if there is money in the wallet is either yes or no.
+*/
 class Currency
 {
 private:
@@ -25,15 +34,11 @@ public:
 	// Constructor
 	Currency(std::string, std::string, unsigned, unsigned);
 	Currency(std::string, std::string);
-
-	// Mutators
 	void setWholeName(std::string);
 	void setFractName(std::string);
 	void setWholeVal(const unsigned);
 	void setFractVal(const unsigned);
 	void setZero();
-
-	// Accessors
 	std::string getWholeName() const;
 	std::string getFractName() const;
 	unsigned getWholeVal() const;
